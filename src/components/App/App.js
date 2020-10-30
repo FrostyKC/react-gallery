ed32e4a48bbe969b720d9aef88479c83d4e34e60
Joe Thompson <smnindian30@yahoo.com>
2020-10-30 16:47:07 -0500

    added pictures to images folder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,9 +8,12 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Gallery of my life</h1>
         </header>
-        <br/>
+        <br />
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/goat_small.jpg" />
+        <img src="images/packers.png" />
+        <img src="images/Old_School_RuneScape_logo.png" />
+        <img src="images/Ezreal.jpg" />
       </div>
     );
   }
